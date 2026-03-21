40d6092892d63516bbc6b0856ebd22d51de3bcd6
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-03-21 11:24:28 -0600

    scaling back

--- assignment2StartingCode/src/utilities/QueueADT.java
+++ assignment2StartingCode/src/utilities/QueueADT.java
@@ -8,24 +8,6 @@ package utilities;
 public interface QueueADT<E> {
 	
 	
-	/**
-	 * The size method will return the current element count contained in the queue.
-	 * 
-	 * @return The current element count.
-	 */
-	public int size();
-	
-	/**
-	 * Removes all of the elements from this queue. This list will be empty after
-	 * this call returns.
-	 */
-	public void clear();
-	
-	/**
-	 * adds new element to bottom of the queue
-	 * 
-	 * @param toADD element to be added
-	 */
 	public void enqueue(E toADD);
 	
 	/**
@@ -43,18 +25,5 @@ public interface QueueADT<E> {
 	 */
 	public E peek();
 	
-	/**
-	 * 
-	 * @param queue2 queue to compare to current queue
-	 * @return returns true if both queues are equal. False if either queue have a difference
-	 */
-	public boolean equals(QueueADT<? extends E> queue2);
-	
-	/**
-	 * Returns true if this list contains no elements. False if it contains elements
-	 * 
-	 * @return true if this list contains no elements. False if it contains elements
-	 */
-	public boolean isEmpty();
 	
 }
