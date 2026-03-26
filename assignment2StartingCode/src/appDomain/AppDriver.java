85c3a7017b373c02529b2ddaee4d0f7aa790504e
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-03-26 12:23:21 -0600

    adding app driver

---
+++ assignment2StartingCode/src/appDomain/AppDriver.java
@@ -0,0 +1,15 @@
+package appDomain;
+
+
+public class AppDriver
+{
+	/**
+	 *  The main method is the entry point of the application.
+	 *  
+	 *  @param args The input to control the execution of the application.
+	 */
+	public static void main( String[] args )
+	{
+	
+	}
+}
