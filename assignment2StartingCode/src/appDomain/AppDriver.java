d3bf3ecfc48cd310ae29a92942f8163defd2f261
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-03-29 14:47:37 -0600

    updated app driver
    loads data, begins parse, has test printing

--- assignment2StartingCode/src/appDomain/AppDriver.java
+++ assignment2StartingCode/src/appDomain/AppDriver.java
@@ -1,5 +1,8 @@
 package appDomain;
 
+import implementations.MyArrayList;
+import implementations.MyStack;
+import utilities.*;
 
 public class AppDriver
 {
@@ -10,6 +13,24 @@ public class AppDriver
 	 */
 	public static void main( String[] args )
 	{
-	
+		String fileName = "";
+		
+		//setting file name for test
+		fileName = "res/sample1.xml";
+		MyArrayList<String> data = LoadData.loadData(fileName);
+		ParseData parseXML = new ParseData(data);
+		
+		//test print
+		MyStack<String> tagStack = parseXML.getTagStack();
+		MyStack<Integer> tagStackLine = parseXML.getTagStackLine();
+		int totalSize = tagStack.size();
+		System.out.println("Size: "+tagStack.size());
+		for(int i = 0; i < totalSize; i++)
+		{
+			// System.out.println("Size: "+tagStack.size());
+			System.out.println("Line #: "+tagStackLine.pop() + " tag: " + tagStack.pop());
+			// System.out.println("Index: " + i);
+		}
+
 	}
 }
