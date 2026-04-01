fee3b363728269f8132bbe7e0b7330ff032a2769
CanOzveren <ozverencan@gmail.com>
2026-04-01 16:15:28 -0600

    Update MyDLLNode.java

--- assignment2StartingCode/src/implementations/MyDLLNode.java
+++ assignment2StartingCode/src/implementations/MyDLLNode.java
@@ -1,12 +1,19 @@
 package implementations;
 
+/**
+ * This class represents a node in a doubly linked list.
+ *
+ * @param <E> the type of data stored in the node
+ */
 public class MyDLLNode<E> {
 	
 	public E data;
 	public MyDLLNode<E> prev;
 	public MyDLLNode<E> next;
 	
-	
+	/**
+	 * Creates a new node with the given data.
+	 */
 	public MyDLLNode(E data) {
 		this.data = data;
 		this.prev = null;
